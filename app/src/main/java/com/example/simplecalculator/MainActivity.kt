48f656829f573771c2c07e0f227bf52c3a3a76fc
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-03-03 17:05:33 +0300

    Apply architect changes

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -5,15 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.simplecalculator.InputStates.DotInputState
 import com.example.simplecalculator.InputStates.NumberInputState
+import com.example.simplecalculator.InputStates.OperatorInputState
 
 var tvOutput: TextView? = null
 var tvOutputHistory: TextView? = null
 
-val expressionStringParser = ExpressionStringParser()
-var currentState: InputState = NumberInputState(expressionStringParser)
+var expressionStringParser: ExpressionStringParser? = null
+var currentState: InputState? = null
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        expressionStringParser = ExpressionStringParser()
+        val notNullExpressionList = expressionStringParser?.mutableExpressionList?: mutableListOf("")
+        val stateList = listOf(NumberInputState(notNullExpressionList),
+            DotInputState(notNullExpressionList), OperatorInputState(notNullExpressionList))
+
+        currentState = NumberInputState(notNullExpressionList, stateList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         val buttonAC = findViewById<Button>(R.id.btnClear)
         buttonAC.setOnClickListener {
             tvOutput?.text = ""
+            expressionStringParser?.mutableExpressionList?.clear()
+            expressionStringParser?.mutableExpressionList?.add("")
         }
 
         tvOutput?.text = ""
@@ -32,10 +46,10 @@ class MainActivity : AppCompatActivity() {
 
     fun onClick(view: View){
         val button = view as Button
-        val stateResponse: Pair<InputState, String> = currentState.isRefersState(button.text.toString())
-        currentState = stateResponse.first
+        val stateResponse = currentState?.getState(button.text.toString())
+        currentState = stateResponse?.first
 
-        tvOutput?.text = tvOutput?.text.toString() + stateResponse.second
-        tvOutputHistory?.text = expressionStringParser.getAnswer()
+        tvOutput?.text = tvOutput?.text.toString() + stateResponse?.second
+        tvOutputHistory?.text = expressionStringParser?.getAnswer()
     }
 }
